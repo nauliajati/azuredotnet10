16a1efb392faa868cffacc2f1bb5a7dc72cc59e4
Ubuntu <vma@VM-A.4wv1s3gjbt2exf0dgrl1esduqc.ix.internal.cloudapp.net>
2018-11-24 14:36:39 +0000

    Folders

---
+++ myApp_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myApp_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kelompok 10");
+        }
+    }
+}
